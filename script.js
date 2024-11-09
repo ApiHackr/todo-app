14a467fd0d14855ca74e0f3e45551ea7c07d87ba
ApiHackr <elz.eth.hacker@gmail.com>
2024-11-09 16:34:13 +0100

    #

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 
 const NEW_TASK_INPUT = document.getElementById("new-task");
 const ADD_TASK_BTN = document.getElementById("add-task-btn");
+const ALL_TASKS = document.getElementById("all-tasks");
+const NOT_DONE = document.getElementById("not-done");
+const COMPLITED = document.getElementById("complited");
+
+function addNewTask() {
+    let inputValue = NEW_TASK_INPUT.value;
+    // ADD_TASK_BTN.onclick = () => {}
+    ALL_TASKS.innerHTML = inputValue;
+}
+addNewTask();
