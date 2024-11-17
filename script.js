057a77ba1b9aa7afea3e35798d153c619cf48ea7
ApiHackr <elz.eth.hacker@gmail.com>
2024-11-17 08:40:55 +0100

    #

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 "use strict";
 
 const NEW_TASK_INPUT = document.getElementById("new-task");
-const ADD_TASK_BTN = document.getElementById("add-task-btn");
+const ADD_TASK_BTN = document.querySelector(".add-task-btn");
 const ALL_TASKS = document.getElementById("all-tasks");
 const NOT_DONE = document.getElementById("not-done");
 const COMPLITED = document.getElementById("complited");
 
 function addNewTask() {
-    let inputValue = NEW_TASK_INPUT.value;
+    const inputValue = NEW_TASK_INPUT.value;
+    const listItem = document.createElement("li");
+    const checkBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
     ADD_TASK_BTN.onclick = () => {
-        ALL_TASKS.innerHTML = `<h1>${inputValue}</h1>`;
+        listItem.classList.add("all");
+        listItem.innerText = inputValue;
+        ALL_TASKS.appendChild(listItem);
+        checkBtn.classList.add("done");
+        checkBtn.innerText = `&check`
+        deleteBtn.classList.add("delete");
+        deleteBtn.innerText = "&cross;"
+        listItem.appendChild(checkBtn);
+        listItem.appendChild(deleteBtn);
+        // ALL_TASKS.innerHTML = `<li>${inputValue}</li>`;
         
     }
 }
 addNewTask();
+
+        // <li class="all">
+        //     New ToDo
+        //     <button class="done">&check;</button>
+        //     <button class="delete">&cross;</button>
+        // </li>
