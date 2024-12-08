db7c8dad44507d0b08782c43e8999f902b55e36f
ApiHackr <elz.eth.hacker@gmail.com>
2024-12-08 21:43:29 +0100

    #

--- script.js
+++ script.js
@@ -28,6 +28,11 @@ function addNewTask() {
     listItem.appendChild(checkBtn);
     listItem.appendChild(deleteBtn);
         // ALL_TASKS.innerHTML = `<li>${inputValue}</li>`;
+    if(listItem.classList[0] === "not-done") {
+        NOT_DONE.appendChild(listItem);
+    } else {
+        COMPLITED.appendChild(listItem);
+    }
 }
 
 
@@ -50,3 +55,63 @@ function removeElement(e) {
         //     Done
         //     <button class="delete">&cross;</button>
         // </li>
+
+
+/* var app = new Vue({
+    el: "#aufgabenApp",
+    data: {
+      aufgaben: JSON.parse(localStorage.getItem('aufgaben')) || [],
+      neueAufgabe: ''
+    },
+    computed: {
+      erledigt() {
+        return this.aufgaben.filter(aufgabe => aufgabe.fertig);
+      },
+      nichterledigt() {
+        return this.aufgaben.filter(aufgabe => !aufgabe.fertig);
+      },
+    },
+    methods: {
+      aufgabeHinzu() {
+        this.aufgaben.push({beschreibung: this.neueAufgabe, fertig: false});
+        this.neueAufgabe = '';
+        localStorage.setItem('aufgaben', JSON.stringify(this.aufgaben));
+      },
+      loeschen(index) {
+        this.aufgaben.splice(index, 1);
+        localStorage.setItem('aufgaben', JSON.stringify(this.aufgaben));
+      },
+      erledigtWechseln(index) {
+        this.aufgaben[index].fertig = !this.aufgaben[index].fertig;
+        localStorage.setItem('aufgaben', JSON.stringify(this.aufgaben));
+      }
+    }
+  });
+
+
+
+<div id="aufgabenApp">
+            <h1>To-do-Liste</h1>
+            <p>
+                <label for="neueAufgabe">Weitere Aufgaben</label>
+                <input type="text" id="neueAufgabe" v-model="neueAufgabe" placeholder="Neue Aufgabe eingeben">
+                <button class="hinzu" @click="aufgabeHinzu">hinzufügen</button>
+            </p>
+            <h1>Alle Aufgaben</h1>
+            <ul>
+                <li v-for="(aufgabe, index) in aufgaben" :class="{ 'abgeschlossen' : aufgabe.fertig}">
+                    <p>{{aufgabe.beschreibung}}</p>
+                    <button class="erledigtWechseln" @click="erledigtWechseln(index)">&#10004;</button>
+                    <button class="loeschen" @click="loeschen(index)">&#10008;</button>
+                </li>
+            </ul>
+            <h2>Noch tun</h2>
+            <ul>
+                <li v-for="aufgabe in nichterledigt">{{ aufgabe.beschreibung}}</li>
+            </ul>
+            <h2>Abgeschlossen</h2>
+            <ul>
+                <li v-for="aufgabe in erledigt">{{ aufgabe.beschreibung}}</li>
+            </ul>
+</div>
+*/
