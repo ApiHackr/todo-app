e09a4ef33364243c8865a79fd010bb23e100ed49
ApiHackr <elz.eth.hacker@gmail.com>
2025-06-15 23:19:35 +0200

    #

--- script.js
+++ script.js
@@ -10,85 +10,16 @@ const tasks = [];
 
 addTaskBtn.addEventListener('click', addTask);
 
-function addTask() {};
+function addTask() {
+  const inputValue = newTask.trim();
+  if (inputValue) {
+    tasks.push({description: inputValue, done: false});
+    // inputValue = '';
+    console.log(tasks);
+  }
+};
 
-// addTaskBtn.addEventListener("click", addNewTask);
-// allTasks.addEventListener("click", removeElement);
-// complited.addEventListener("click", removeElement);
-
-// function addNewTask() {
-//     const inputValue = newTaskInput.value;
-//     const listItem = document.createElement("li");
-//     listItem.classList.add("all");
-//     listItem.innerText = inputValue;
-//     const checkBtn = document.createElement("button");
-//     checkBtn.classList.add("done");
-//     checkBtn.innerHTML = "&check;"
-//     const deleteBtn = document.createElement("button");
-//     deleteBtn.classList.add("delete");
-//     deleteBtn.innerHTML = "&cross;";
-    
-//     allTasks.appendChild(listItem);
-//     listItem.appendChild(checkBtn);
-//     listItem.appendChild(deleteBtn);
-//     allTasks.innerHTML = `<li>${inputValue}</li>`;
-// }
-
-
-// function removeElement(e) {
-//     const item = e.target;
-
-//     if(item.classList[0] === "delete") {
-//         const toRemove = item.parentElement;
-//         toRemove.remove();
-//     }
-// }
-        // <li class="all">
-        //     New ToDo
-        //     <button class="done">&check;</button>
-        //     <button class="delete">&cross;</button>
-        // </li>
-
-
-        // <li class="complited">
-        //     Done
-        //     <button class="delete">&cross;</button>
-        // </li>
-
-
-/* var app = new Vue({
-    el: "#aufgabenApp",
-    data: {
-      aufgaben: JSON.parse(localStorage.getItem('aufgaben')) || [],
-      neueAufgabe: ''
-    },
-    computed: {
-      erledigt() {
-        return this.aufgaben.filter(aufgabe => aufgabe.fertig);
-      },
-      nichterledigt() {
-        return this.aufgaben.filter(aufgabe => !aufgabe.fertig);
-      },
-    },
-    methods: {
-      aufgabeHinzu() {
-        this.aufgaben.push({beschreibung: this.neueAufgabe, fertig: false});
-        this.neueAufgabe = '';
-        localStorage.setItem('aufgaben', JSON.stringify(this.aufgaben));
-      },
-      loeschen(index) {
-        this.aufgaben.splice(index, 1);
-        localStorage.setItem('aufgaben', JSON.stringify(this.aufgaben));
-      },
-      erledigtWechseln(index) {
-        this.aufgaben[index].fertig = !this.aufgaben[index].fertig;
-        localStorage.setItem('aufgaben', JSON.stringify(this.aufgaben));
-      }
-    }
-  });
-
-
-
+/*
 <template>
   <div id="aufgaben-app">
     <h1>To-do-Liste</h1>
@@ -206,5 +137,81 @@ export default {
     text-decoration: line-through;
   }
 </style>
+*/
+
+// addTaskBtn.addEventListener("click", addNewTask);
+// allTasks.addEventListener("click", removeElement);
+// complited.addEventListener("click", removeElement);
+
+// function addNewTask() {
+//     const inputValue = newTaskInput.value;
+//     const listItem = document.createElement("li");
+//     listItem.classList.add("all");
+//     listItem.innerText = inputValue;
+//     const checkBtn = document.createElement("button");
+//     checkBtn.classList.add("done");
+//     checkBtn.innerHTML = "&check;"
+//     const deleteBtn = document.createElement("button");
+//     deleteBtn.classList.add("delete");
+//     deleteBtn.innerHTML = "&cross;";
+    
+//     allTasks.appendChild(listItem);
+//     listItem.appendChild(checkBtn);
+//     listItem.appendChild(deleteBtn);
+//     allTasks.innerHTML = `<li>${inputValue}</li>`;
+// }
+
+
+// function removeElement(e) {
+//     const item = e.target;
+
+//     if(item.classList[0] === "delete") {
+//         const toRemove = item.parentElement;
+//         toRemove.remove();
+//     }
+// }
+        // <li class="all">
+        //     New ToDo
+        //     <button class="done">&check;</button>
+        //     <button class="delete">&cross;</button>
+        // </li>
+
+
+        // <li class="complited">
+        //     Done
+        //     <button class="delete">&cross;</button>
+        // </li>
+
+
+/* var app = new Vue({
+    el: "#aufgabenApp",
+    data: {
+      aufgaben: JSON.parse(localStorage.getItem('aufgaben')) || [],
+      neueAufgabe: ''
+    },
+    computed: {
+      erledigt() {
+        return this.aufgaben.filter(aufgabe => aufgabe.fertig);
+      },
+      nichterledigt() {
+        return this.aufgaben.filter(aufgabe => !aufgabe.fertig);
+      },
+    },
+    methods: {
+      aufgabeHinzu() {
+        this.aufgaben.push({beschreibung: this.neueAufgabe, fertig: false});
+        this.neueAufgabe = '';
+        localStorage.setItem('aufgaben', JSON.stringify(this.aufgaben));
+      },
+      loeschen(index) {
+        this.aufgaben.splice(index, 1);
+        localStorage.setItem('aufgaben', JSON.stringify(this.aufgaben));
+      },
+      erledigtWechseln(index) {
+        this.aufgaben[index].fertig = !this.aufgaben[index].fertig;
+        localStorage.setItem('aufgaben', JSON.stringify(this.aufgaben));
+      }
+    }
+  });
 
 */
